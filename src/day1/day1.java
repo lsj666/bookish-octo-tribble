ddbc6bbc31c72fbc08a9aaa6d462359cccc51d50
lsj <879059341@qq.com>
2017-05-08 20:53:16 +0800

    init project.

--- src/day1/day1.java
+++ src/day1/day1.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 import org.junit.Test;
 
-public class hello {
-
+public class Day1 {
 	@Test
 	public void test1() {
 
 		int year = 2017;
-		if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+		if (((year % 4 == 0) && (year % 100 != 0)) 
+				|| (year % 400 == 0)) {
 			System.out.println("是闰年");
 		} else {
 			System.out.println("不是闰年");
@@ -32,11 +32,6 @@ public class hello {
 	       System.out.println("after: " + num1);
 	       System.out.println("after: " + num2);
 	}
-	
-	
-	
-	
-	
 	
 	
 	
@@ -64,6 +59,78 @@ public class hello {
 	    } else {
 	        System.out.println("不及格");
 	    }
-
 	}
+
+
+	@Test
+	public void test4(){
+		int num = 1;//1、循环条件的初始化
+		while (num <= 5){//2、循环条件的判断
+			num++;//3、循环条件的改变（趋向于循环条件
+			System.out.println("HelloWorld!");
+		}
+	}
+	
+	@Test
+	public void test5(){
+		int num = 1;//1、循环条件的初始化
+		int a = 0;
+		while (num <= 100){//2、循环条件的判断
+			System.out.println(num);
+			
+			a = a + num;//a += num
+			num++;//3、循环条件的改变（趋向于循环条件
+		}
+		System.out.println("a:" + a);
+	}
+	
+	
+	@Test
+	public void for1(){
+		for (int i = 1; i <= 5; i++){
+			System.out.println("HelloWord!");
+		}
+	}
+	
+	@Test
+	public void for2(){
+		int a = 0;
+		for (int i = 1; i <= 100; i++){
+			a = a + i;
+			//System.out.println(i);
+		}
+		System.out.println("a:" + a);
+	}
+	
+	@Test
+	public void for3(){
+		int a = 0;
+		for (int i = 1; i <= 100; i ++){
+			if (i % 7 == 0){
+				System.out.println(i);
+				a = a + 1;
+			}
+		}
+		System.out.println("一共有" + a +"个");
+	}
+	
+	public void for4(){
+		for (int i = 0; i <= 3; i ++){
+			for (int j = 1;j <= 3; j ++){
+				System.out.print("*");
+				
+			}
+		}
+	}
+	
+	@Test
+	public void for5(){
+		for (int i=1;i<=9;i++) {
+			for (int j=1;j<=i;j++){
+				System.out.print(i+"*"+j+"="+(i*j)+"\t");
+			}
+			System.out.print("\n");
+		}
+	}
+	
 }
